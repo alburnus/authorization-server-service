2ec7afa015d9f54e8cb96ae818f51bbc81e07b88
Krzysztof <k.ukleja@gmail.com>
2018-10-09 23:12:33 +0200

    OAuth with github

--- src/main/java/pl/alburnus/AuthorizationServerApplication.java
+++ src/main/java/pl/alburnus/AuthorizationServerApplication.java
@@ -2,8 +2,10 @@ package pl.alburnus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
